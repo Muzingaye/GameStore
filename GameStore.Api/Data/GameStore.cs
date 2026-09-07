7033fe2a16d44e57b63030bac7da69863bede2f3
Muzingaye <muzingaye.dube@gmail.com>
2026-09-07 09:51:52 +0200

    migrations

--- GameStore.Api/Data/GameStore.cs
+++ GameStore.Api/Data/GameStore.cs
@@ -1,8 +1,11 @@
+using GameStore.Api.EndPoints;
+using GameStore.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
 
-class GameStoreContext : DbContext
+class GameStoreContext(DbContextOptions<GameStoreContext> opt) : DbContext(opt)
 {
-
+    public DbSet<Game> Games => Set<Game>();
+    public DbSet<Genre> Genres => Set<Genre>();
 }
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,8 +1,13 @@
+using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.EndPoints;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddValidation();
+
+var connString = "Data Source=GamesStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 app.MapGameEndPoints();
 app.Run();
