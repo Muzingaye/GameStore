906977cc09eef9e8b283f8503e7caa88d1a86325
Muzingaye <muzingaye.dube@gmail.com>
2026-09-07 10:33:49 +0200

    db

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,13 +1,15 @@
 using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.EndPoints;
+using GameStore.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddValidation();
 
-var connString = "Data Source=GamesStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connString);
+builder.AddGamesStore();
 
 var app = builder.Build();
 app.MapGameEndPoints();
+
+app.MigrateDb();
 app.Run();
